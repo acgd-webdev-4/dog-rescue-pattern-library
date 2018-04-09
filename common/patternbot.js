38f294e46ccbc0953009079b5801bf4fbfc46799
Paulinamissio <paolinamissio@Paolinas-MacBook-Pro.local>
2018-04-09 07:34:40 -0400

    finished checkout

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/paolinamissio/Desktop/AC Graphic Design/Year 2/Web Development/ecommerce-pattern-library
- * @version 1523212189584
+ * @version 1523212367070
  */
-const patternManifest_1523212189583 = {
+const patternManifest_1523212367070 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -943,5 +943,5 @@ const patternManifest_1523212189583 = {
   }
 };
 
-patternBotIncludes(patternManifest_1523212189583);
+patternBotIncludes(patternManifest_1523212367070);
 }());
